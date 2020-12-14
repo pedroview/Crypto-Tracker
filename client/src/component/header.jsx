9bdc8a52d5be7ad587bb067897aff2bc61b82da1
pedromass <maduekwepedro@gmail.com>
2020-12-15 00:33:43 +0100

    removed some images from home page

--- client/src/component/header.jsx
+++ client/src/component/header.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
-export default ({ symbols }) => {
+export default ({ symbols: propsymbols }) => {
+  const symbols = propsymbols.filter((x) => !["LINK", "DOT", "BSV", "CRO", "USDC", "LEO"].includes(x));
+
   return (
     <div className="crypto-header">
       <div>
         {symbols.map((symbol, index) => {
-          if (index <= 9) return <img key={index} src={`/img/crypto/${symbol}.png`} className="indiv-img" alt={symbol} />;
+          if (index < 5) return <img key={index} src={`/img/crypto/${symbol}.png`} className="indiv-img" alt={symbol} />;
         })}
       </div>
       <h1>Crypto Tracker</h1>
       <div>
         {symbols.map((symbol, index) => {
-          if (index > 9) return <img key={index} src={`/img/crypto/${symbol}.png`} className="indiv-img" alt={symbol} />;
+          if (index >= 5 && index <= 7) return <img key={index} src={`/img/crypto/${symbol}.png`} className="indiv-img" alt={symbol} />;
         })}
       </div>
     </div>
